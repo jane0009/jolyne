eeab6c6c032dacb4c131215006c538f7a93892c9
Jane Petrovna <jane@j4.pm>
2024-04-29 17:51:04 -0400

    example setup

---
+++ build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.dependency("jolibs", .{
+      .target = target,
+      .optimize = optimize
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "jolyne",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("jolibs", lib.module("jolibs"));
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Start the server");
+    run_step.dependOn(&run_cmd.step);
+
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    tests.root_module.addImport("jolibs", lib.module("jolibs"));
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_tests.step);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# jolyne
+
+this is my repo for playing around with zig's web hosting and/or backend capabilities. hyper experimental, do not use. requires zig 0.12.x
